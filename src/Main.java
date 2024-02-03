761471d45ab9b25dcc81969b6f3b4796134b5912
Elvin Abaev <elvinabaev@gmail.com>
2024-02-03 22:42:18 +0300

    Methods in Java.

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int firstResult = calculateScore(true, 800, 5, 200);
+        int secondResult = calculateScore(false, 302, 210, 211);
+        showResults(firstResult, secondResult);
+    }
 
-        // ------------- 1 --------------------
-        boolean gameOver = true;
-        int score = 800;
-        int levelCompleted = 5;
-        int bonus = 100;
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        int finalScore = 0;
 
         if (gameOver) {
-            int finalScore = score + (levelCompleted * bonus);
+            finalScore = score + (levelCompleted * bonus);
             System.out.println("Your final score = " + finalScore);
         }
+        return finalScore;
+    }
 
-        // ------------- 2 --------------------
-        boolean newGameOver = true;
-        int newScore = 10000;
-        int newLevelCompleted = 8;
-        int newBonus = 200;
-
-        if (newGameOver) {
-            int finalScore = newScore + (newLevelCompleted * newBonus);
-            System.out.println("Your final score = " + finalScore);
-        }
+    public static void showResults(int firstResult, int secondResult) {
+        System.out.printf("First result = %d, second result = %d", firstResult, secondResult);
     }
 }
